1164197b2c8685d84116a923e4ab402be9d00c44
Niminraj7 <nniminraj@gmail.com>
2024-12-30 21:40:57 +0530

    Add files via upload

---
+++ 1_guessingGame.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+   int n, rando;
+   int count = 0;
+   printf("Welcome to the guessing game.\n ");
+   srand(time(NULL)); //seed
+
+   //generating a random number between 1 to 500 
+   rando = rand() % 500 +1;
+
+   do{
+    printf("\nEnter your number(1 to 500): ");
+    scanf("%d",&n);
+    count++;
+
+    if(n>rando){
+        printf("\nGo for smaller number.");
+    }
+
+    else if(n<rando){
+        printf("\nGo for larger number. ");
+    }
+
+    else{
+        printf("\nCongratulations! You guessed the number in %d attempts.",count);
+    }
+   } while(n!=rando); 
+
+    printf("\nIt was a wonderfull session. Hoping to see you again. bye bye.");
+    printf("\nDeveloped by Niminraj.");
+
+    return 0;
+}
